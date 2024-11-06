14b63fdd50d0a825d0b91faa09a5309bb425b5c5
nabankur007 <nabankur787@gmail.com>
2024-11-06 11:42:55 +0530

    haiodhoadnoawidhnopaiwkd

---
+++ notes/src/main/java/com/secure/notes/controllers/AuditLogController.java
@@ -0,0 +1,30 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.models.AuditLog;
+import com.secure.notes.services.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/audit")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600, allowCredentials="true")
+public class AuditLogController {
+    @Autowired
+    AuditLogService auditLogService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<AuditLog> getAuditLogs(){
+        return auditLogService.getAllAuditLogs();
+    }
+
+    @GetMapping("/note/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<AuditLog> getNoteAuditLogs(@PathVariable Long id){
+        return auditLogService.getAuditLogsForNoteId(id);
+    }
+
+}
